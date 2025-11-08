70123a791164007924169cb22360795eef1901fa
hrishikesh8904 <hrishikesh.sane89@outlook.com>
2025-11-08 23:48:52 +0530

    error11

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://slack-online-chat-application-4.onrender.com",
+    ],
+    credentials: true,
   },
 });
 
